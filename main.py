e46d9763a9c682fd0bd8c877308d8d342ba4a99e
MichaelChatt <michael.chattington@gmail.com>
2026-08-17 16:53:06 +0100

    A: Added basic screen

---
+++ main.py
@@ -0,0 +1,17 @@
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+import pygame
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True == True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
