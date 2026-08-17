0293cbaa98baf99fd6d04852c35cffab4ed92c06
MichaelChatt <michael.chattington@gmail.com>
2026-08-17 17:40:27 +0100

    C: Added player icon

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 )
     while True == True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        player.draw(screen)
         dt = clock.tick(60) / 1000
 
 
